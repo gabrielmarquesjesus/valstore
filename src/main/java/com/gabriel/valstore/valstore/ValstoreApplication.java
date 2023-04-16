14cf46e52e65e0ad19b4531f4a564f77643c0560
Gabrielt <gabrielmarques@compels.net>
2023-04-16 11:26:24 -0300

    first commit

---
+++ src/main/java/com/gabriel/valstore/valstore/ValstoreApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.valstore.valstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/valstore/valstore/ValstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.valstore.valstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
